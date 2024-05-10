cac8aa2fa88cf634be42e89d99201e8adf6868aa
JeffreyLee143 <66732562+JeffreyLee143@users.noreply.github.com>
2024-05-10 19:23:26 +0800

    2024_05_10
    連續行對話待解決

--- test.py
+++ test.py
@@ -4,16 +4,16 @@ from linebot.webhook import WebhookHandler
 from linebot import LineBotApi
 from linebot.v3.exceptions import InvalidSignatureError
 from linebot.models import *
-import re
+#import re
 import json
-import pandas as pd
+#import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.image as img
-import os
-import getpass
+#import os
+#import getpass
 import db_cmd
-import csv
-import pymysql
+#import csv
+#import pymysql
 import json
 db_settings_file=open('src\db_setting.json','r')
 db_settings=json.load(db_settings_file)
@@ -63,20 +63,60 @@ def member_informaton():
             handle_message(event)
         db_cmd.add_member(msg[0],msg[1],msg[2])
 
+def view_menu():
+    @handler.add(MessageEvent, message=TextMessage)
+    def handle_message_view_menu(event):
+        message_text = event.message.text
+        if(message_text.isdigit() == False or 7<int(message_text) or int(message_text)<1):
+            #line_bot_api.reply_message(event.reply_token,TextMessage(text='請輸入正確的數字'))
+            print("err")
+        image_message = db_cmd.view_menu(message_text)
+        menu=ImageSendMessage(
+              original_content_url=image_message,
+              preview_image_url=image_message
+            )
+        #line_bot_api.reply_message(event.reply_token,menu)
+        print('2')
+    return
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message_text = event.message.text
     reply_token = event.reply_token
-    # 這裡是點餐系統的處理邏輯
+    #case "營業時間"
     if(message_text=="營業時間"):
-            line_bot_api.reply_message(event.reply_token,TextMessage(text=db_cmd.run_time()))
-        #case "立即點餐":
-        #case "查看菜單":
-        #case "優惠資訊":
-            
-    #if msg[0] == '新增菜單':
-    #    member_informaton()
-        #db_cmd.add_member(msg[1],msg[2],msg[3])
+        line_bot_api.reply_message(event.reply_token,TextMessage(text=db_cmd.run_time()))
+        return
+    #case "查看菜單"
+    if(message_text=="查看菜單"):
+        print("查看菜單")
+        menu = "輸入數字來選擇想要查看的店家菜單：\n1. 早安美廣\n2. 傳香飯糰\n3. 八方雲集\n4. 宜廷小吃\n5. 琪美食堂\n6. 美廣鮮果吧\n7. 自助餐"
+        #line_bot_api.reply_message(reply_token, TextSendMessage(text=menu))
+        view_menu()
+        return
+    #case "立即點餐"
+    if(message_text=="立即點餐"):
+        print("立即點餐")
+        return
+    #case "優惠資訊":
+    if(message_text=="優惠資訊"):
+        print("優惠資訊")
+        return
 
+#加入好友後建立會員資料進資料庫
+@handler.add(FollowEvent)
+def handle_follow(event):
+    user_id=event.source.user_id
+    profile = line_bot_api.get_profile(user_id)
+    db_cmd.add_member(profile.user_id,profile.display_name)
+
+#刪除好友後資料庫刪除會員資料
+@handler.add(UnfollowEvent)
+def handle_unfollow(event):
+    user_id=event.source.user_id
+    profile = line_bot_api.get_profile(user_id)
+    db_cmd.delete_member(profile.user_id)
+
+#啟動服務
 if __name__ == "__main__":
     app.run()

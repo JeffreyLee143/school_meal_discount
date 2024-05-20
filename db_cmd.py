d6aea5b2ae837ed9fcfa3f7ed4f3418f4a3b8d2c
JeffreyLee143 <66732562+JeffreyLee143@users.noreply.github.com>
2024-05-20 09:04:02 +0800

    2024_05_20
    2024_05_20

--- db_cmd.py
+++ db_cmd.py
@@ -2,27 +2,30 @@
 import csv
 import pymysql
 import json
+import math
+import time
+import datetime
 db_settings_file=open('src\db_setting.json','r')
 db_settings=json.load(db_settings_file)
 
-def add_member(member_number,member_name,member_mail):
+def add_member(member_id,member_name):
     try:
         conn = pymysql.connect(**db_settings)
         with conn.cursor() as cursor:
-            command = "INSERT INTO `members`(member_number,member_name,member_mail)VALUE(%s,%s,%s)"
+            command = "INSERT INTO `members`(member_id,member_name)VALUE(%s,%s)"
             cursor.execute(
-                command, (member_number,member_name,member_mail)
+                command, (member_id,member_name)
             )
             conn.commit()
     except Exception as ex:
         print(ex)
 
-def delete_member(member_mail):
+def delete_member(member_id):
     try:
         conn = pymysql.connect(**db_settings)
         with conn.cursor() as cursor:
-            command = "DELETE FROM `members` WHERE member_mail = %s"
-            cursor.execute(command,(member_mail))
+            command = "DELETE FROM `members` WHERE member_id = %s"
+            cursor.execute(command,(member_id))
             conn.commit()
     except Exception as ex:
         print(ex)
@@ -61,15 +64,75 @@ def add_order(shop_id,product_name,total_cost):
     except Exception as ex:
         print(ex)
 
-def delete_order(order_id,shop_id):
+def delete_order(order_id,time):
     try:
         conn = pymysql.connect(**db_settings)
         with conn.cursor() as cursor:
-            command = "DELETE FROM `order_histories` WHERE order_id = %s AND shop_id= %s"
+            command = "SELECT trade_date FROM `order_histories` WHERE order_id = %s"
             cursor.execute(
-                command, (int(order_id),int(shop_id))
+                command, (int(order_id),)
             )
-            conn.commit()
+            datetime1 = cursor.fetchone()[0]
+            datetime2 = datetime.datetime.fromtimestamp(time/1000)
+            timedelta = datetime2 - datetime1
+            if(timedelta > datetime.timedelta(minutes=1)):
+                message='店家已經接收並開始製作訂單\n故無法取消'
+                return message
+            elif(timedelta<=datetime.timedelta(minutes=1)):
+                command = "DELETE FROM `order_histories` WHERE order_id = %s"
+                cursor.execute(
+                    command, (int(order_id),)
+                )
+                conn.commit()
+                message='刪除成功'
+                return message
+    except Exception as ex:
+        print(ex)
+
+def view_order(order_id):
+    try:
+        conn = pymysql.connect(**db_settings)
+        with conn.cursor() as cursor:
+            command = "SELECT * FROM `order_histories` WHERE order_id=%s"
+            cursor.execute(
+                command, (int(order_id))
+            )
+            if len(cursor.fetchall()) == 0:
+                return '沒有此訂單，請確認訂單id是否有打對\n'
+            else:
+                str=[]
+                for i in cursor._rows:
+                    str.append(i[1])
+                    str.append(i[3])
+                    str.append(i[4])
+                menu = order_name(str[0], str[1], str[2])
+                return menu
+    except Exception as ex:
+        print(ex)
+
+def order_name(shop_id, product_name, product_number):
+    try:
+        conn = pymysql.connect(**db_settings)
+        with conn.cursor() as cursor:
+            command = "SELECT shop_name FROM `shops` WHERE shop_id=%s"
+            cursor.execute(command, (int(shop_id),))
+            result=[]
+            result.append(cursor.fetchone()[0])
+            if(len(product_name)>1):
+                name_num=product_name.split(',')
+                num=product_number.split(',')
+            else:
+                name_num=product_name
+                num=product_number
+            for i in range(len(name_num)):
+                command = "SELECT product_name FROM `menu` WHERE product_id IN (%s)"
+                cursor.execute(command,(int(name_num[i])))
+                result.append(cursor.fetchone()[0])
+            for i in range(len(num)):
+                result[i+1] += '*'
+                result[i+1] += num[i]
+            menu = '\n'.join(result)
+            return menu
     except Exception as ex:
         print(ex)
 
@@ -85,6 +148,21 @@ def add_menu(shop_id,product_name,product_price,count):
     except Exception as ex:
         print(ex)
 
+def view_menu(shop_id):
+    try:
+        conn = pymysql.connect(**db_settings)
+        with conn.cursor() as cursor:
+            command = "SELECT * FROM `shops` WHERE shop_id = %s"
+            cursor.execute(
+                command, (shop_id)
+            )
+            str=''
+            for i in cursor:
+                str=i[4]
+            return str
+    except Exception as ex:
+        print(ex)
+
 def run_time():
     try:
         conn = pymysql.connect(**db_settings)
@@ -94,11 +172,160 @@ def run_time():
             run_time_dic={}
             str=''
             for i in cursor:
-                str=str+i[0]+' : '+ i[1] + '\n'
+                str+=i[0]+' : '+ i[1] + '\n'
                 run_time_dic[i[0]]=i[1]
+            str = str.rstrip()
             return str
     except Exception as ex:
         print(ex)
+
+def order_detail(msg):
+    msg=msg.split('\n')
+    shop_name=msg[0]
+    shop_id=check_shop_id(shop_name)[0]
+    product_name=''
+    product_number=''
+    total_cost=0
+    for row in msg[1:]:
+        row=row.split(',')
+        prouct_detail=check_product(shop_id,row[0])
+        product_name+=str(prouct_detail[0])+','
+        product_number+=row[1]+','
+        total_cost+=float(prouct_detail[1])*float(row[1])
+    round(total_cost,0)
+    product_name = product_name.rstrip(',')
+    product_number = product_number.rstrip(',')
+    order_detail={
+        'shop_id':str(shop_id),'product_name':str(product_name),'product_number':str(product_number),'total_cost':str(int(total_cost))
+    }
+    return order_detail
+
+def check_product(shop_id,product_name):
+    try:
+        conn = pymysql.connect(**db_settings)
+        with conn.cursor() as cursor:
+            command = "SELECT * FROM `menu` WHERE shop_id = %s AND product_name = %s"
+            cursor.execute(
+                command, (int(shop_id),product_name)
+            )
+            str=[]
+            for i in cursor:
+                str.append(i[1])
+                str.append(i[3])
+            return str
+    except Exception as ex:
+        print(ex)
+
+def check_shop_id(shop_name):
+    try:
+        conn = pymysql.connect(**db_settings)
+        with conn.cursor() as cursor:
+            command = "SELECT * FROM `shops` WHERE shop_name = %s"
+            cursor.execute(
+                command, (shop_name)
+            )
+            str=[]
+            for i in cursor:
+                str.append(i[0])
+            return str
+    except Exception as ex:
+        print(ex)
+
+def order_now(msg):
+    order=order_detail(msg)
+    try:
+        conn = pymysql.connect(**db_settings)
+        with conn.cursor() as cursor:
+            command = "INSERT INTO `order_histories`(shop_id,product_name,product_number,total_cost)VALUE(%s,%s,%s,%s)"
+            cursor.execute(
+                command, (order['shop_id'],order['product_name'],order['product_number'],order['total_cost'])
+            )
+            conn.commit()
+            command = "SELECT `order_id` FROM `order_histories` WHERE shop_id = %s AND product_name = %s ORDER BY `order_id` DESC LIMIT 1"
+            cursor.execute(
+                command, (order['shop_id'],order['product_name'])
+            )
+            for i in cursor:
+                return str(i[0])
+    except Exception as ex:
+        print(ex)
+
+def discount_information():
+    try:
+        conn = pymysql.connect(**db_settings)
+        with conn.cursor() as cursor:
+            command = "SELECT discount_content FROM discounts;"
+            cursor.execute(command)
+            str=''
+            for i in cursor:
+                str+=i[0]+ '\n'
+            str = str.rstrip()
+            return str
+    except Exception as ex:
+        print(ex)
+
+def add_discount(id,content,discount):
+    try:
+       conn = pymysql.connect(**db_settings)
+       cursor = conn.cursor()
+       with conn.cursor() as cursor:
+            command = "INSERT INTO `discount`(id,content,discount)VALUE(%s,%s,%s)"
+            cursor.execute(
+                command, (id,content,discount)
+            )
+            conn.commit()
+    except Exception as ex:
+        print(ex)
+
+def delete_discount(id):
+    try:
+        conn = pymysql.connect(**db_settings)
+        with conn.cursor() as cursor:
+            command = "DELETE FROM `discount` WHERE id = %s"
+            cursor.execute(command,(id))
+            conn.commit()
+    except Exception as ex:
+        print(ex)
+
+def add_point(id,price):
+    try:
+       member_point=price/50 #設定每消費50元可獲得一點
+       conn = pymysql.connect(**db_settings)
+       cursor = conn.cursor()
+       with conn.cursor() as cursor:
+            command =  "UPDATE members SET member_point = member_point+%s WHERE member_id = %s"
+            cursor.execute(
+                command, (int(member_point),id)
+            )
+            conn.commit()
+    except Exception as ex:
+        print(ex)
+
+def spend_point(member_id, member_point):
+    try:
+        conn = pymysql.connect(**db_settings)
+        with conn.cursor() as cursor:
+            # 查詢指定會員號的資料
+            cursor.execute("SELECT member_point FROM members WHERE member_id = %s", (member_id,))
+            result = cursor.fetchone()
+            if result is None:
+                print("Member not found.")
+                return
+            # 獲取並檢查當前的積分
+            current_point = result[0]  # 使用索引0來獲取'point'欄位的值
+            if current_point == 0:
+                print('This member has no points')
+                return
+            if current_point < member_point:
+                print('Not enough points')
+                return
+            # 更新積分
+            command = "UPDATE members SET member_point = member_point - %s WHERE member_id = %s"
+            cursor.execute(command, (member_point, member_id))
+            conn.commit()
+    except Exception as ex:
+        print(ex)
+
 '''
 import chardet
 with open('src\menu.csv', 'rb') as f:
@@ -106,18 +333,13 @@ with open('src\menu.csv', 'rb') as f:
 encoding = result['encoding']
 with open("src\menu.csv",newline='',encoding=encoding) as csvfile:
     count = 1
+    shop_id=1
     menu = csv.DictReader(csvfile)
     for product in menu:
+        if(int(product['shop_id'])!=shop_id):
+            count=1
+            shop_id+=1
         add_menu(int(product['shop_id']),product['product_name'],float(product['cost']),count)
         count+=1
 '''
-
-
-#shop_name = {"早安美廣","傳香飯糰","八方雲集","宜廷小吃","琪美食堂","美廣鮮果吧","自助餐"}
-#for i in shop_name:
-#    add_shop(i)
-#add_member("09123456789","Jeffrey","anb@gmail.com")
-#delete_member("anb@gmail.com")
-#delete_shop("big_shop@gmail.com")
-#add_order(1,"'蛋餅',2,'巧克力',3",5000)
-#delete_order(1,1)
+add_point('U5331a53d095a53d040a15a0481c6945c',150)
---
+++ query1.sql
@@ -0,0 +1,74 @@
+use school_meal_discount;
+CREATE TABLE `members`(
+	`member_id` VARCHAR(45) NOT NULL PRIMARY KEY,
+    `member_type` boolean DEFAULT FALSE,
+    `member_registration_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `member_name` VARCHAR(45) 
+);
+
+CREATE TABLE `shops`(
+	`shop_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `shop_registration_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `shop_name` VARCHAR(45) ,
+    `shop_run_time` VARCHAR(11),
+    `shop_menu_url` VARCHAR(70)
+);
+
+CREATE TABLE `menu`(
+	`shop_id` INT NOT NULL,
+    `product_id` INT,
+    `product_name` VARCHAR(45) NOT NULL,
+    `product_price` DOUBLE NOT NULL,
+    PRIMARY KEY(`shop_id`,`product_id`),
+    FOREIGN KEY (`shop_id`) REFERENCES `shops`(`shop_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE `order_histories`(
+	`order_id` INT NOT NULL AUTO_INCREMENT,
+    `shop_id` INT,
+    `trade_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `product_name` VARCHAR(310) ,
+    `product_number` VARCHAR(310) ,
+    `total_cost` INT ,
+    `order_status` INT DEFAULT 0,
+    PRIMARY KEY(`order_id`,`shop_id`),
+    FOREIGN KEY (`shop_id`) REFERENCES `shops`(`shop_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE `manager`(
+	`manager_id` INT NOT NULL AUTO_INCREMENT,
+    `manager_mail` VARCHAR(45) 
+);
+
+CREATE TABLE `discounts`(
+ discount_id INT AUTO_INCREMENT PRIMARY KEY,
+ discount_content VARCHAR(50),
+ discount_discount FLOAT
+);
+
+ALTER TABLE shops ADD shop_menu_url VARCHAR(70);
+DROP TABLE `discounts`;
+DROP TABLE `members`;
+DROP TABLE `shops`;
+DROP TABLE `menu`;
+DROP TABLE `order_histories`;
+DESCRIBE `members`;
+DELETE FROM `members`;
+DELETE FROM `order_histories`
+WHERE order_id = 1 AND shop_id = 1;
+
+DELETE FROM `shops`
+WHERE shop_id < 8;
+INSERT INTO `discounts`(discount_content,discount_discount)VALUE('95折',0.95);
+INSERT INTO `discounts`(discount_content,discount_discount)VALUE('9折',0.90);
+INSERT INTO `discounts`(discount_content,discount_discount)VALUE('85折',0.85);
+INSERT INTO `discounts`(discount_content,discount_discount)VALUE('8折',0.8);
+ALTER TABLE `members` ADD member_point INT DEFAULT 0;
+ALTER TABLE `members` DROP COLUMN member_point;
+INSERT INTO `shops`(shop_id,shop_name,shop_run_time,shop_menu_url)VALUE('1','早安美廣','07:00~16:00','https://i.postimg.cc/mZjPk5QG/2024-05-09-203153.jpg');
+INSERT INTO `shops`(shop_id,shop_name,shop_run_time,shop_menu_url)VALUE('2','傳香飯糰','07:00~19:30','https://i.postimg.cc/mDQ1dQjb/2024-05-03-225704.jpg');
+INSERT INTO `shops`(shop_id,shop_name,shop_run_time,shop_menu_url)VALUE('3','八方雲集','11:00~19:30','https://i.postimg.cc/3xSqRGgJ/2024-05-03-225723.jpg');
+INSERT INTO `shops`(shop_id,shop_name,shop_run_time,shop_menu_url)VALUE('4','宜廷小吃','11:00~19:30','https://i.postimg.cc/RZxLnJmS/19d872c0-4906-42b2-a283-ae6882ee8443.jpg');
+INSERT INTO `shops`(shop_id,shop_name,shop_run_time,shop_menu_url)VALUE('5','琪美食堂','11:00~19:30','https://i.postimg.cc/Pf6nQZrM/0dabfdda-0f3d-430d-b974-d6c1c746c1e8.jpg');
+INSERT INTO `shops`(shop_id,shop_name,shop_run_time,shop_menu_url)VALUE('6','美廣鮮果吧','11:00~19:30','https://i.postimg.cc/SKTcrkjR/2024-05-03-224705.jpg');
+INSERT INTO `shops`(shop_id,shop_name,shop_run_time)VALUE('7','自助餐','11:00~19:30');
